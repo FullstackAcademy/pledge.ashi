d741a92325db7cf80f998bfef173c32e5b90d2a1
Gabriel Lebec <glebec@gmail.com>
2014-11-30 13:39:02 -0500

    initialized

---
+++ vow.js
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------
+Promises Workshop: build the vow.js deferral-style promise library
+----------------------------------------------------------------*/
+// YOUR CODE HERE:
+
+
+
+
+
+
+
+/*-------------------------------------------------------
+The spec was designed to work with Test'Em, so we don't
+actually use module.exports. But here it is for reference:
+
+module.exports = {
+  defer: defer,
+  all: all
+};
+
+So in a Node-based project we could write things like this:
+
+var vow = require('vow');
+var myDefer1 = vow.defer();
+var myPledge1 = myDefer1.pledge;
+…
+var pledgeForResults = vow.all([pledge1, pledge2]);
+--------------------------------------------------------*/
